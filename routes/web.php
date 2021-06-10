f569df20a72f1ac374084a15239c30fdd2b7325d
akiko2103 <akik0066aki@gmail.com>
2021-06-10 17:42:54 +0000

    課題提出0611

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,30 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+// /*
+// |--------------------------------------------------------------------------
+// | Web Routes
+// |--------------------------------------------------------------------------
+// |
+// | Here is where you can register web routes for your application. These
+// | routes are loaded by the RouteServiceProvider within a group which
+// | contains the "web" middleware group. Now create something great!
+// |
+// */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+//カリキュラム13で修正したので元のやつコメント化0611
+// Route::group(['prefix' => 'admin'], function() {
+//     Route::get('news/create', 'Admin\NewsController@add');
+// });
+
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+     Route::get('news/create', 'Admin\NewsController@add');
+     Route::post('news/create', 'Admin\NewsController@create'); # 追記
 });
 
 /*課題3*/
@@ -24,17 +32,26 @@ Route::group(['prefix' => 'admin'], function() {
 Route::get('XXX', 'Admin\AAAController@BBB');
 
 /*課題4*/
-Route::get('admin/profile/create','Admin\ProfileController@add');
+//カリキュラム13で修正したので元のやつコメント化0611
+//Route::get('admin/profile/create','Admin\ProfileController@add');
 
-Route::get('admin/profile/edit','Admin\ProfileController@edit');
-
-/*
-     またはgroup化して以下のコードです。
-    Route::group(['prefix'=>'admin'],function(){
-    Route:get('profile/create','Admin\ProfileController@add');
-    Route:get('profile/edit','Admin\ProfileController@edit');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+Route::get('profile/create','Admin\ProfileController@add');
+Route::post('profile/create', 'Admin\ProfileController@create');
 });
-*/
+
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+Route::get('profile/edit','Admin\ProfileController@edit');
+Route::post('profile/edit', 'Admin\ProfileController@edit');
+
+// /*
+//      またはgroup化して以下のコードです。
+//     Route::group(['prefix'=>'admin'],function(){
+//     Route:get('profile/create','Admin\ProfileController@add');
+//     Route:get('profile/edit','Admin\ProfileController@edit');
+// });
+// */
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
@@ -44,6 +61,8 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('profile/create', 'Admin\NewsController@add')->middleware('auth');
     Route::get('profile/edit', 'Admin\NewsController@add')->middleware('auth');
 });
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+});
