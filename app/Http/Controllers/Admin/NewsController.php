497d8e6e4a9eb3ee17c9d0b88dc0568b4c086b93
akiko2103 <akik0066aki@gmail.com>
2021-06-15 10:54:48 +0000

    一時保存0615

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use App\News;
-
+use App\History;
+use Carbon\Carbon;
 
 class NewsController extends Controller
 {
@@ -87,6 +88,13 @@ public function update(Request $request)
       unset($news_form['_token']);
       // 該当するデータを上書きして保存する
       $news->fill($news_form)->save();
+      
+      $history = new History;
+      $history->news_id = $news->id;
+      $history->edited_at = Carbon::now();
+      $history->save();
+      
+      
       return redirect('admin/news');
   }
   
